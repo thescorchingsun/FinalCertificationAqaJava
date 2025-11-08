01a579554c1e9f027209b72fbf44e283511f4632
Nina Petrova <endtoend8@yandex.ru>
2025-11-08 22:43:21 +0200

    yml

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -1,13 +1,30 @@
 package pages;
 
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 
 public class BasePage {
 
     protected WebDriver driver;
+    private final int DEFAULT_TIMEOUT = 20;
 
     public BasePage(WebDriver driver) {
         this.driver = driver;
+        waitForPageToLoad();
+    }
+
+    /**
+     * Метод ожидает полной загрузки страницы (document.readyState == "complete")
+     */
+    protected void waitForPageToLoad() {
+        new WebDriverWait(driver, Duration.ofSeconds(DEFAULT_TIMEOUT)).until(
+                webDriver -> ((JavascriptExecutor) webDriver)
+                        .executeScript("return document.readyState")
+                        .equals("complete")
+        );
     }
 }
